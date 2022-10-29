bab61ca8d35ac44331c855d887a3e6a6ba297d89
zebbyjz <megajehanzaib@gmail.com>
2022-10-29 08:26:53 +0300

    Formatting

--- client/src/Pages/UserListing.jsx
+++ client/src/Pages/UserListing.jsx
@@ -18,7 +18,9 @@ function UserListing() {
           setData(res.data);
           setLoading(false);
         })
-        .catch((error) => console.log(error.message));
+        .catch((error) => {
+          console.log(error.message);
+        });
     };
 
 
@@ -43,6 +45,7 @@ function UserListing() {
     const handleSearch=()=>{
         setChecked(false)
         fetchSearchData(search)
+        console.log("HIT")
 
     }
 
@@ -62,76 +65,118 @@ function UserListing() {
             <SideBar></SideBar>
             
             <div className="d-grid m-5 justify-content-center" style={{ width: "100%" }}>
-                <h1>Fetching Data</h1>
+                <h1 >Fetching Data</h1>
             </div>
             </div>
     </React.Fragment>)}
 
-    else{return ( 
-        <React.Fragment>
-            <div className="d-flex flex-row">
-            <SideBar></SideBar>
-            
-            <div className="d-grid m-5" style={{ width: "100%" }}>
-            
+    else{return (
+      <React.Fragment>
+        <div className="d-flex flex-row">
+          <SideBar></SideBar>
 
+          <div className="d-grid m-5" style={{ width: "100%" }}>
             <div className="accordion w-100 " id="accordionExample">
-            <div className="accordion-item shadow-lg">
+              <div className="accordion-item shadow-lg">
                 <h2 className="accordion-header" id="headingOne">
-                    <button className="w-100 accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Display Users (CLICK HERE)
-                    </button>
+                  <button
+                    className="w-100 accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    Click to Display Users
+                  </button>
                 </h2>
-                <div className="accordion-collapse collapse show" id="collapseOne">
-                    <div className="accordion-body">
-                        <ul className="pt-5" style={{width:"75vw"}}>
-                            <div className="form-check ms-3 mb-3">
-                                <input className="form-check-input mt-1" type="checkbox" value="" id="flexCheckChecked" checked={isChecked} onChange={handleCheckboxToggle}/>
-                                <label  className='form-check-label' htmlFor="flexCheckChecked">Show All Records</label>
-                            </div>
-                            
-                            <div className='d-flex flex-row'>
-                                <input type="text" className='form-control ms-3 me-5' placeholder='Search for specific name' value={search} onChange={(event)=>{onTextChange(event)}}/>
-                                <button className='btn btn-primary me-5' style={{width:"12em"}} onClick={handleSearch}><i className="bi bi-search pe-none me-2"></i>Search</button>
-                            </div>
-                            <hr/>
-                            <div className='MYTABLE'>
-                                <table className="table table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th className='text-center'>ID</th>
-                                            <th className='text-center'>Name</th>
-                                            <th className='text-center'>Email</th>
-                                            <th className='text-center'>Cell Number</th>
-                                            <th className='text-center'>Created At</th>
-                                            <th className='text-center'>Is Deleted</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody className='table-group-divider'>
-                                        {data.map( (record)=>{
-                                            return <tr key={record._id}>
-                                            <td className='text-center'>{record._id}</td>
-                                            <td className='text-center'>{record.Name}</td>
-                                            <td className='text-center'>{record.Email}</td>
-                                            <td className='text-center'>{record["Mobile Number"]}</td>
-                                            <td className='text-center'>{new Date(record.createdAt).toLocaleString()}</td>
-                                            <td className='text-center'>{"No"}</td>
-                                            </tr>
-                                        } )}
-                                    </tbody>
-                                </table>
-                            </div>
-                        </ul>
-                    </div>
+                <div
+                  className="accordion-collapse collapse show"
+                  id="collapseOne"
+                >
+                  <div className="accordion-body">
+                    <ul className="pt-5" style={{ width: "75vw" }}>
+                      <div className="form-check ms-3 mb-3">
+                        <input
+                          className="form-check-input mt-1"
+                          type="checkbox"
+                          value=""
+                          id="flexCheckChecked"
+                          checked={isChecked}
+                          onChange={handleCheckboxToggle}
+                        />
+                        <label
+                          className="form-check-label"
+                          htmlFor="flexCheckChecked"
+                        >
+                          Show All Records
+                        </label>
+                      </div>
+
+                      <div className="d-flex flex-row">
+                        <input
+                          type="text"
+                          className="form-control ms-3 me-5"
+                          placeholder="Search for specific name"
+                          value={search}
+                          onChange={(event) => {
+                            onTextChange(event);
+                          }}
+                        />
+                        <button
+                          className="btn btn-primary me-5"
+                          style={{ width: "12em" }}
+                          onClick={handleSearch}
+                        >
+                          <i className="bi bi-search pe-none me-2"></i>Search
+                        </button>
+                      </div>
+                      <hr />
+                      <div className="MYTABLE">
+                        <table className="table table-bordered">
+                          <thead>
+                            <tr>
+                              <th className="text-center">ID</th>
+                              <th className="text-center">Name</th>
+                              <th className="text-center">Email</th>
+                              <th className="text-center">Cell Number</th>
+                              <th className="text-center">Created At</th>
+                              <th className="text-center">Is Deleted</th>
+                            </tr>
+                          </thead>
+                          <tbody className="table-group-divider">
+                            {data.map((record) => {
+                              return (
+                                <tr key={record._id}>
+                                  <td className="text-center">{record._id}</td>
+                                  <td className="text-center">{record.Name}</td>
+                                  <td className="text-center">
+                                    {record.Email}
+                                  </td>
+                                  <td className="text-center">
+                                    {record["Mobile Number"]}
+                                  </td>
+                                  <td className="text-center">
+                                    {new Date(
+                                      record.createdAt
+                                    ).toLocaleString()}
+                                  </td>
+                                  <td className="text-center">{"No"}</td>
+                                </tr>
+                              );
+                            })}
+                          </tbody>
+                        </table>
+                      </div>
+                    </ul>
+                  </div>
                 </div>
+              </div>
             </div>
-
-            </div>
-
-            </div>
-            </div>
-        </React.Fragment>
-     );}
+          </div>
+        </div>
+      </React.Fragment>
+    );}
 }
 
 export default UserListing;
